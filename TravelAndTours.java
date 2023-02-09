88e552761f56afd7ffaa25f930f5809de429b0d2
njc <caneteneiljason71967@gmail.com>
2023-02-09 15:46:09 +0800

    push

--- TravelAndTours.java
+++ TravelAndTours.java
@@ -34,7 +34,7 @@ public class TravelAndTours {
         ret = scanner.nextLine();
         return ret;
     }
-    
+
     static void getch() {
         println("");
         print("Press Enter to continue...");
@@ -63,6 +63,13 @@ public class TravelAndTours {
         return selection(title, false, options);
     }
 
+    static char confirmation(String title) {
+        char answer = inputString(title + "(y/N)").strip().charAt(0);
+        if (answer == 'y' || answer == 'Y') {
+          return true;
+        return false;
+    }
+
     public static void main(String[] args) {
         String tourist = "";
         boolean isDone = false;
@@ -82,6 +89,7 @@ public class TravelAndTours {
                 "12-Day Adventure & Sightseeing Vacation Package to Boracay, Cebu & Bohol with Flights from Manila",
                 "4D3N Siargao Package with Airfare | Himaya Resort from Manila + Island Hopping Tour"
             );
+            Tourist touristDetails;
             /* Package Details */
             switch (packageChoice) {
                 case 'a':
@@ -98,23 +106,123 @@ public class TravelAndTours {
                     println("\t-", "Access to swimming pool and selected resort facilities");
                     println("\t-", "All environmental, port fees, and all other applicable taxes");
                     println("\tPRICE:", "PHP", "20,359.00");
-                    print("Confirm selected package? (y/N) ");
-                    char c = new Scanner(System.in).nextLine().strip().charAt(0);
-                    if (!(c == 'y' || c == 'Y'))
+                    boolean c = confirmation("Confirm selected package? (y/N) ");
+                    if (c)
                         println("Cancelled.");
                     else {
-                        
+                        touristDetails = new Tourist(20,359.00);
+                        touristDetails.register();
                     }
                     break;
                 case 'b':
+                    println("4D3N Bohol Package with Airfare | Mithi Resort from Manila:");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\tPRICE:", "PHP", ".00");
+                    boolean c = confirmation("Confirm selected package?");
+                    if (c)
+                        println("Cancelled.");
+                    else {
+                        touristDetails = new Tourist(20,359.00);
+                        touristDetails.register();
+                    }
                     break;
                 case 'c':
+                    println("4D3N Bohol Package with Airfare | Mithi Resort from Manila:");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\tPRICE:", "PHP", ".00");
+                    boolean c = confirmation("Confirm selected package?");
+                    if (c)
+                        println("Cancelled.");
+                    else {
+                        touristDetails = new Tourist(20,359.00);
+                        touristDetails.register();
+                    }
                     break;
                 case 'd':
+                    println("4D3N Bohol Package with Airfare | Mithi Resort from Manila:");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\tPRICE:", "PHP", ".00");
+                    boolean c = confirmation("Confirm selected package?");
+                    if (c)
+                        println("Cancelled.");
+                    else {
+                        touristDetails = new Tourist(20,359.00);
+                        touristDetails.register();
+                    }
                     break;
                 case 'e':
+                    println("4D3N Bohol Package with Airfare | Mithi Resort from Manila:");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\tPRICE:", "PHP", ".00");
+                    boolean c = confirmation("Confirm selected package?");
+                    if (c)
+                        println("Cancelled.");
+                    else {
+                        touristDetails = new Tourist(20,359.00);
+                        touristDetails.register();
+                    }
                     break;
                 case 'f':
+                    println("4D3N Bohol Package with Airfare | Mithi Resort from Manila:");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\t-", "");
+                    println("\tPRICE:", "PHP", ".00");
+                    boolean c = confirmation("Confirm selected package?");
+                    if (c)
+                        println("Cancelled.");
+                    else {
+                        touristDetails = new Tourist(20,359.00);
+                        touristDetails.register();
+                    }
                     break;
                 default:
                     isDone = true;
@@ -124,4 +232,44 @@ public class TravelAndTours {
         println("Thank You and Goodbye,", tourist, "!");
     }
 
+}
+
+
+class Tourist {
+    private double price;
+    private String[] tourists;
+    private int touristCount;
+    Tourist(double price) {
+      this.price = price;
+      this.touristCount = 0;
+    }
+    public double getPrice() {
+      return Math.round(this.price * 100) * 0.01;
+    }
+    public int getNumberOfTourist() {
+      return this.touristCount;
+    }
+    public double getTotalPrice() {
+      return Math.round(this.touristCount * this.price * 100) * 0.01;
+    }
+    public boolean register() {
+      int touristCount = Integer.parseInt(TravelAndTours.inputString("How many tourist will be registering?").strip());
+      if (touristCount < 1) {
+          TravelAndTours.println("Invalid number of tourists!");
+          return false;
+      }
+      this.touristCount = touristCount;
+      TravelAndTours.println("Total Price: PHP", this.getTotalPrice().toString());
+      boolean confirm = TravelAndTours.confirmation("Confirm package?");
+      if (!confirm) {
+          this.touristCount = 0;
+          return false;
+      }
+      TravelAndTours.println("Registration: (", this.touristCount.toString(), "Tourists)");
+      tourist = new String[touristCount];
+      for (int i = 0; i < touristCount; i++) {
+        tourists[i] = TravelAndTours.inputString((i+1) + ")");
+      }
+      return true;
+    }
 }
